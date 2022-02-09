8de49c21f014712ab633c80e12f4a94922783e1f
surpriso1997 <maneesh10chaulagain@gmail.com>
2022-02-09 21:12:16 +0545

    created route constants for the app

---
+++ newproj/lib/navigation/routes.dart
@@ -0,0 +1,8 @@
+class Routes {
+  static const String profileRoute = "/profieagwseaweglee";
+  static const String settingsRoute = "/settings";
+  static const String homeRoute = "/home";
+  static const String dashboardRoute = "/dashboard";
+  static const String searchRoute = "/search";
+  static const String loginRoute = "/login";
+}
