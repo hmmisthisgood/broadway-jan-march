d5b9c01a1f81dd99a06ed05d8eb4cb33f76bbe70
surpriso1997 <maneesh10chaulagain@gmail.com>
2022-02-22 22:00:12 +0545

    added load more posts function in photos cubit

--- 8pm/http_adv/lib/bloc/photos_bloc.dart
+++ 8pm/http_adv/lib/bloc/photos_bloc.dart
@@ -4,6 +4,7 @@ import 'dart:convert';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:http_adv/bloc/photos_state.dart';
+import 'package:http_adv/model/photo.dart';
 import 'package:http_adv/util/constants.dart';
 import 'package:http/http.dart' as apiClient;
 import 'package:logger/logger.dart';
@@ -15,9 +16,9 @@ class PhotosCubit extends Cubit<PhotosState> {
 
   final logger = Logger();
 
-  deletePost() {}
-  updatePost() {}
+  int currentPage = 1;
 
+  List<Photo> allPhotos = [];
   // return
 // from inside bloc/cubit functions, we [emit]
 
@@ -26,7 +27,7 @@ class PhotosCubit extends Cubit<PhotosState> {
 
     // logger.i("Fethinvg data from server");
     final url =
-        "https://pixabay.com/api/?key=${Constants.API_KEY}&q=cats&image_type=photo";
+        "https://pixabay.com/api/?key=${Constants.API_KEY}&q=cats&image_type=photo&per_page=5";
     // logger.i(url);
 
     final uri = Uri.parse(url);
@@ -35,9 +36,47 @@ class PhotosCubit extends Cubit<PhotosState> {
 
       final body = response.body;
 
-      final decodedJson = json.decode(body);
+      final Map<String, dynamic> decodedJson = json.decode(body);
+      List hits = decodedJson['hits'];
 
-      emit(SuccessState(data: body));
+      final photosData = hits.map((item) {
+        final convertedPhoto = Photo.convertMapToPhoto(item);
+        return convertedPhoto;
+      }).toList();
+      allPhotos.addAll(photosData);
+      emit(SuccessState(data: photosData));
+    } catch (e) {
+      // logger.e(e);
+
+      emit(ErrorState(errorMessage: e.toString()));
+    }
+  }
+
+  loadMorePosts() async {
+    print("loading more posts");
+    currentPage++;
+
+    emit(LoadingState());
+
+    // logger.i("Fethinvg data from server");
+    final url =
+        "https://pixabay.com/api/?key=${Constants.API_KEY}&q=cats&image_type=photo&page=$currentPage&per_page=5";
+
+    final uri = Uri.parse(url);
+    try {
+      final response = await apiClient.get(uri);
+      final Map<String, dynamic> decodedJson = json.decode(response.body);
+
+      List newHits = decodedJson['hits'];
+
+      /// new 20 allPhotos
+      final loadedMorePhotos = newHits.map((item) {
+        final convertedPhoto = Photo.convertMapToPhoto(item);
+        return convertedPhoto;
+      }).toList();
+      allPhotos.addAll(loadedMorePhotos);
+
+      emit(SuccessState(data: allPhotos));
     } catch (e) {
       // logger.e(e);
 
@@ -45,9 +84,3 @@ class PhotosCubit extends Cubit<PhotosState> {
     }
   }
 }
-
-// class PhotosCubit extends Cubit<PhotosState> {
-//   PhotosCubit() : super(new InitialState());
-
-
-// }
